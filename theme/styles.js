d3047bed088a7fadb36189bbfa22b722edf3c66d
mpere <mpereannor@gmail.com>
2021-08-19 08:54:26 +0100

    feat: implement theme v2

--- theme/styles.js
+++ theme/styles.js
@@ -2,11 +2,19 @@ const styles = {
     global: {
         html: { 
             bg: "sand.50",
-            color: "gray.900"
+            color: "gray.900",
+            padding: "0",
+            margin: "0"
         },
         body: { 
             bg: "sand.50",
-            color: "gray.900"
+            color: "gray.900",
+            padding: "0",
+            margin: "0"
+        },
+        a: { 
+            color: "inherit",
+            textDecoration: "none"
         },
         "*": {
             boxSizing: "border-box",
